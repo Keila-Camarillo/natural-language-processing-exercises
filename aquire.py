00f98468ccb8dddf2b6f6e6da46cee19b940322f
Keila-Camarillo <126615506+Keila-Camarillo@users.noreply.github.com>
2023-06-20 16:23:26 -0500

    exercise blog and articles

--- aquire.py
+++ aquire.py
@@ -33,23 +33,26 @@ def get_blog_articles(url='https://codeup.com/blog/'):
     return article_dict
 
 def get_news_articles(url="https://blog.inshorts.com/"):
-    soup = create_soup(url)
+    pages = [f"{url}page/{i}/" for i in range(2,7)]
+    for p in pages:
+        soup = create_soup(p)
     
-    # list of links from website
-    links_list = [i.find("a")["href"] for i in soup.find_all("h2", class_="entry-title")]
-    category_list = [i["class"][7] for i in soup.find_all("article")]
-    article_dict = []
-    for link in links_list:
-        # get access to pages
-        soup = create_soup(link)
-        # title
-        title = soup.h1.text
-        # content info
-        text_list = [p.get_text(strip=True) for div in soup.find_all("div", class_="entry-content") for p in div.find_all("p")]
-        # list of category names
-        category_list = [i["class"][6][9:] for i in soup.find_all("article")]
-        # append dict to list
-        
-        content_dict = {"title": title, "content": text_list, "category": category_list}
-        article_dict.append(content_dict)
+  
+        # list of links from website
+        links_list = [i.find("a")["href"] for i in soup.find_all("h2", class_="entry-title")]
+        category_list = [i["class"][7] for i in soup.find_all("article")]
+        article_dict = []
+        for link in links_list:
+            # get access to pages
+            soup = create_soup(link)
+            # title
+            title = soup.h1.text
+            # content info
+            text_list = [p.get_text(strip=True) for div in soup.find_all("div", class_="entry-content") for p in div.find_all("p")]
+            # list of category names
+            category_list = [i["class"][6][9:] for i in soup.find_all("article")]
+            # append dict to list
+
+            content_dict = {"title": title, "content": text_list, "category": category_list}
+            article_dict.append(content_dict)
     return article_dict
